694b6274832294a57a218e9a9c15dcf61852e117
mirza <mirza.akhena@gmail.com>
2022-05-22 10:03:11 +0700

    adding plain log

--- README.md
+++ README.md
@@ -22,4 +22,7 @@ We have 4 name convention for usecase
   This name is for do some action or task or command
 
 
+https://itnext.io/golang-1-18-generics-the-good-the-bad-the-ugly-5e9fa2520e76
+https://stackoverflow.com/questions/15715605/multiple-goroutines-listening-on-one-channel
+
 GOOS=windows GOARCH=amd64 go build
--- command/genopenapi/command.go
+++ command/genopenapi/command.go
@@ -14,17 +14,22 @@ type ObjTemplate struct {
 }
 
 type OASSchema struct {
-	Type       string `json:"type"`
-	Properties []any  `json:"properties"`
-	Required   bool   `json:"required"`
+	Ref        string   `json:"$ref,omitempty"`
+	Type       string   `json:"type,omitempty"`
+	Properties []any    `json:"properties,omitempty"`
+	Required   bool     `json:"required,omitempty"`
+	Default    string   `json:"default,omitempty"`
+	Enum       []string `json:"enum"`
 }
 
 type OASParameter struct {
-	Name        string    `json:"name"`
-	In          string    `json:"in"`
-	Description string    `json:"description"`
-	Required    bool      `json:"required"`
-	Schema      OASSchema `json:"schema"`
+	Name        string                `json:"name"`
+	In          string                `json:"in"`
+	Description string                `json:"description"`
+	Required    bool                  `json:"required"`
+	Schema      OASSchema             `json:"schema"`
+	Example     any                   `json:"example,omitempty"`
+	Examples    map[string]OASExample `json:"examples,omitempty"`
 }
 
 type OASExample struct {
@@ -35,25 +40,25 @@ type OASExample struct {
 }
 
 type OASMediaType struct {
-	Schema   string                `json:"schema,omitempty"`
+	Schema   OASSchema             `json:"schema,omitempty"`
 	Example  any                   `json:"example,omitempty"`
 	Examples map[string]OASExample `json:"examples,omitempty"`
 }
 
 type OASRequestBody struct {
-	Description string         `json:"description,omitempty"`
-	Content     map[string]any `json:"content"`
-	Required    bool           `json:"required,omitempty"`
+	Description string                  `json:"description,omitempty"`
+	Content     map[string]OASMediaType `json:"content"`
+	Required    bool                    `json:"required,omitempty"`
 }
 
 type OASResponse struct {
-	Description string                  `json:"description"`
+	Description string                  `json:"description,omitempty"`
 	Content     map[string]OASMediaType `json:"content"`
 }
 
 type OASOperationObject struct {
 	Tags        []string               `json:"tags"`
-	Summary     string                 `json:"summary"`
+	Summary     string                 `json:"summary,omitempty"`
 	OperationID string                 `json:"operationId"`
 	Parameters  []OASParameter         `json:"parameters"`
 	RequestBody OASRequestBody         `json:"requestBody"`
@@ -78,17 +83,10 @@ type OASExternalDocumentation struct {
 }
 
 type OASServerVariable struct {
-	Description string   `json:"description"`
+	Description string   `json:"description,omitempty"`
 	Default     string   `json:"default"`
 	Enum        []string `json:"enum,omitempty"`
 }
-
-//type OpenAPIVariables struct {
-//	Protocol    OASServerVariable `json:"protocols"`
-//	Environment OASServerVariable `json:"environment"`
-//	Port        OASServerVariable `json:"port"`
-//	BasePath    OASServerVariable `json:"basePath"`
-//}
 
 type OASServer struct {
 	Description string                       `json:"description"`
@@ -96,51 +94,61 @@ type OASServer struct {
 	Variables   map[string]OASServerVariable `json:"variables"`
 }
 
-type OpenAPILicense struct {
+type OASLicense struct {
 	Name string `json:"name"`
 	Url  string `json:"url"`
 }
 
-type OpenAPIContact struct {
+type OASContact struct {
 	Name  string `json:"name"`
 	Email string `json:"email"`
 	Url   string `json:"url"`
 }
 
-type OpenAPIInfo struct {
-	Title          string         `json:"title"`
-	Version        string         `json:"version"`
-	Description    string         `json:"description"`
-	TermsOfService string         `json:"termsOfService"`
-	Contact        OpenAPIContact `json:"contact"`
-	License        OpenAPILicense `json:"license"`
+type OASInfo struct {
+	Title          string     `json:"title"`
+	Version        string     `json:"version"`
+	Description    string     `json:"description"`
+	TermsOfService string     `json:"termsOfService"`
+	Contact        OASContact `json:"contact"`
+	License        OASLicense `json:"license"`
 }
 
-type OpenAPIRoot struct {
+type OASSecurityScheme struct {
+}
+
+type OASComponent struct {
+	SecuritySchemes map[string]OASSecurityScheme `json:"securitySchemes"`
+	Parameters      map[string]OASParameter      `json:"parameters,omitempty"`
+	Schemes         map[string]OASSchema         `json:"schemes"`
+}
+
+type OASRoot struct {
 	OpenAPI      string                   `json:"openapi"`
-	Info         OpenAPIInfo              `json:"info"`
+	Info         OASInfo                  `json:"info"`
 	Servers      []OASServer              `json:"servers"`
 	ExternalDocs OASExternalDocumentation `json:"externalDocs"`
-	Paths        map[string]OASPathItem
+	Paths        map[string]OASPathItem   `json:"paths"`
+	Components   map[string]OASComponent  `json:"components"`
 }
 
 func Run(inputs ...string) error {
 
 	domainName := utils.GetDefaultDomain()
 
-	data := OpenAPIRoot{
+	data := OASRoot{
 		OpenAPI: "3.0.3",
-		Info: OpenAPIInfo{
+		Info: OASInfo{
 			Title:          "Application Restful API",
 			Version:        "1",
 			Description:    "Application Restful API",
 			TermsOfService: "https://gogen.com/tnc",
-			Contact: OpenAPIContact{
+			Contact: OASContact{
 				Name:  "Mirza Akhena",
 				Email: "mirza.akhena@gmail.com",
 				Url:   "mirzaakhena.com",
 			},
-			License: OpenAPILicense{
+			License: OASLicense{
 				Name: "APACHE 2.0",
 				Url:  "https://www.apache.org/licenses/LICENSE-2.0",
 			},
@@ -148,7 +156,7 @@ func Run(inputs ...string) error {
 		Servers: []OASServer{
 			{
 				Description: fmt.Sprintf("%s", domainName),
-				Url:         "https://localhost:8080/api/v1",
+				Url:         "{protocol}://{environment}:{port}/api/{version}",
 				Variables: map[string]OASServerVariable{
 					"protocol": {
 						Description: "Protocol",
@@ -165,27 +173,70 @@ func Run(inputs ...string) error {
 						Default:     "8080",
 						Enum:        []string{"8080", "8081", "8082", "80", "443"},
 					},
-					"basepath": {
-						Description: "Base Path",
+					"version": {
+						Description: "Version",
 						Default:     "v1",
 						Enum:        nil,
 					},
 				},
-				//Variables: OpenAPIVariables{
-				//	Environment: OASServerVariable{
-				//		Description: "Environment",
-				//		Default:     "localhost",
-				//		Enum:        []string{"localhost", "dev", "qa", "prod"},
-				//	},
-
-				//},
 			},
 		},
 		ExternalDocs: OASExternalDocumentation{
 			Description: fmt.Sprintf("Documentation for %s", domainName),
 			URL:         fmt.Sprintf("https://bitbucket.org/%s", domainName),
 		},
-		Paths: map[string]OASPathItem{},
+		Paths:      map[string]OASPathItem{},
+		Components: map[string]OASComponent{},
+	}
+	data.Paths["/products"] = OASPathItem{
+		Get: OASOperationObject{
+			Tags:        []string{"product"},
+			Summary:     "Query All Products",
+			OperationID: "GetAllProduct",
+			Parameters: []OASParameter{
+				{
+					Name:        "id",
+					In:          "query",
+					Description: "product id",
+					Required:    false,
+					Schema: OASSchema{
+						Type: "string",
+					},
+					Example: "PRD1234001",
+				},
+			},
+			Responses: map[string]OASResponse{
+				"200": {
+					Description: "",
+					Content: map[string]OASMediaType{
+						"application/json": {
+							Schema: OASSchema{
+								Type: "string",
+							},
+						},
+					},
+				},
+			},
+		},
+		Post: OASOperationObject{
+			Tags:        []string{"product"},
+			Summary:     "Insert New Product",
+			OperationID: "RunProductCreate",
+			Parameters:  nil,
+			RequestBody: OASRequestBody{
+				Description: "",
+				Content:     nil,
+				Required:    false,
+			},
+			Responses: nil,
+		},
+		Put:         OASOperationObject{},
+		Delete:      OASOperationObject{},
+		Summary:     "",
+		Description: "",
+		Ref:         "",
+		Servers:     OASServer{},
+		Parameters:  OASParameter{},
 	}
 
 	file, err := json.MarshalIndent(data, "", " ")
